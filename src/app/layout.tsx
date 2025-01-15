04e0129802de3a4871fa1d23ff9913ef2bd174f5
matt <mundellm99@gmail.com>
2025-01-15 06:29:54 -0700

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Lasting Buys | Products That Last Forever',
   description: 'Get daily recommendations for products that never break.',
+  icons: {
+    icon: '/favicon.png',
+    // You can also add these for mobile devices
+    apple: '/apple-icon.png',
+    shortcut: '/favicon.png'
+  },
 }
 
 export default function RootLayout({
