15bc7defd03146bea8d07b18dd5a17f2aca493d9
matt <mundellm99@gmail.com>
2025-08-25 21:20:48 -0700

    Updated image sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,8 +75,8 @@ export default function LandingPage() {
             <Image
               src="/makers-on-mainstreet-logo.jpg"
               alt="Makers on Mainstreet Logo"
-              width={25}
-              height={100}
+              width={75}
+              height={150}
               className="mx-auto"
             />
             <p className="text-xl text-gray-600">
